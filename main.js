0db9ea2b0b850fbb7510997c970c4e6a544d9f3b
Maria <lu_vinas@hotmail.com>
2021-05-28 14:38:22 -0400

    most functionality in place

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
     const leftArrow = $('.arrowLeft')
     const first = $('.containerSlider').children().first();
     const last = $('.containerSlider').children().last();
+    const home = $('#home');
     
-    
-    if(first.hasClass('active')) {
-        leftArrow.css('display', 'none')
-        rightArrow.css('display', 'none')
+    if(home.hasClass('active')) {
+        hideArrows();
     }
     
     // Coffee Pop Up 
@@ -36,46 +35,39 @@ $(document).ready(function(){
     closePopUp.on('click', hidePopUp);
 
 
-    // go to selected coffee
+    // hide all slides
+    const allSlides = $('.containerSlider').children()
 
-    // hide home page
     function hideAll() {
         hidePopUp();
-        $('.home').css('display', 'none');
-        $('.slide1').css('display', 'none');
-        $('.slide2').css('display', 'none');
-        $('.slide3').css('display', 'none');
-        $('.slide4').css('display', 'none');
-        $('.slide5').css('display', 'none');
-
+        allSlides.css('display', 'none');
+        allSlides.removeClass('active');
     }
 
 
-    // coffee options- menu
-    $('.option1').on('click', function(){
-        hideAll();
-        $('.slide1').css('display', 'block');
-    })
+    // Go to selected slide
+    const option = $('#coffeeTypes').children();
+    
+    const slide = $('.containerSlider').children();
 
-    $('.option2').on('click', function(){
-        hideAll();
-        $('.slide2').css('display', 'block');
-    })
+    
+    let selectedOption;
+    
+    // selected coffee type
+    option.on('click', function(e){
+        selectedOption = e.target.innerHTML;
 
-    $('.option3').on('click', function(){
-        hideAll();
-        $('.slide3').css('display', 'block');
+        // Check if slide id matches id of selected coffee
+        for(i = 0; i < slide.length; i++) {
+            if(slide[i].id === selectedOption){
+                console.log(slide[i].id)
+                hideAll();
+                showArrows();
+                slide[i].style.display = 'block';
+                slide[i].classList.add('active');
+            }
+        }
     })
-
-    $('.option4').on('click', function(){
-        hideAll();
-        $('.slide4').css('display', 'block');
-    })
-    $('.option5').on('click', function(){
-        hideAll();
-        $('.slide5').css('display', 'block');
-    })
-
 
     // Show arrows
     function showArrows() {
@@ -95,15 +87,14 @@ $(document).ready(function(){
 
     // Next Slide - Right
     function nextSlide(){
-        const currentSlide = $('.active');
+        let currentSlide = $('.active');
         const nextSlide = currentSlide.next();
 
         if(nextSlide.length){
             showArrows()
             currentSlide.removeClass('active').css('display', 'none');
             nextSlide.addClass('active').css('display', 'block');
-        } 
-        else {
+        } else {
             currentSlide = $('.home')
             currentSlide.addClass('active').css('display', 'block');
             hideArrows();
@@ -115,14 +106,19 @@ $(document).ready(function(){
     $('.prev').on('click', prevSlide);
     
     function prevSlide() {
-        const currentSlide = $('.active');
+        let currentSlide = $('.active');
         const prevSlide = currentSlide.prev();
         
         if(prevSlide.length){         
             currentSlide.removeClass('active').css('display', 'none');
             prevSlide.addClass('active').css('display', 'block');
-            } 
+        }  else {
+            currentSlide = $('.home')
+            currentSlide.addClass('active').css('display', 'block');
+            hideArrows();
         }
+        }
+
 
     //  more info
     const info = $('.fa-info');
@@ -151,54 +147,13 @@ $(document).ready(function(){
     closeVideo.on('click', hideVideo);
 
 
-    // $('.fa-video').on('click', function(){
-    //     $('.videoPopup').show();
-    //     $('.description').hide();
-    //     $('.icon').hide();
-    //     $('.next').hide();
-    //     $('.prev').hide();
-    //     $('h2').hide();
 
-    // }); 
-    
-
-    // // close video
-    // $('.close').on('click', function(){
-    //     $('.videoPopup').hide();
-    //     $('.description').show();
-    //     $('.icon').show();
-    //     $('.next').show();
-    //     $('.prev').show();
-    //     $('h2').show();
-    // });
-
-
-
-    // more info- show
-//     $('.fa-info').on('click', function(){
-//         $('.moreInfo').show();
-//         $('.description').hide();
-//         $('.icon').hide();
-//         $('.next').hide();
-//         $('.prev').hide();
-//         $('h2').hide();
-
-
-//     $('.closeInfo').on('click', function(){
-//         $('.moreInfo').hide();
-//         $('.description').show();
-//         $('.icon').show();
-//         $('.next').show();
-//         $('.prev').show();
-//         $('h2').show();
-//     })
-// });
-
-    // listen to a click event that will take the user back to the home page
+    // click event that will take the user back to the home page
     $('.fa-home').on('click', function(){
         $('.home').addClass('active').css('display', "block");
     });
     
+
     
 }); 
 
